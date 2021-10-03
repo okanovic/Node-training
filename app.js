e4bf8def2df7484e04198a5033bd8e79f2a98e53
Okan TOPER <okan.toper94@gmail.com>
2021-10-04 01:21:25 +0300

    edit

--- app.js
+++ app.js
@@ -4,30 +4,17 @@ const debug = require("debug")("app");
 const morgan = require("morgan");
 const path = require("path");
 
+
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-const sessionsRouter = express.Router();
+const sessionsRouter = require('./src/routers/sessionsRouter');
 
 app.use(morgan("tiny"));
 app.use(express.static(path.join(__dirname, '/public/')))
 
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
-
-sessionsRouter.route('/').get((req, res) => {
-    res.render("sessions", {
-        sessions: [
-            { title: "Sessions-1", description: "Sessions-1 Description" },
-            { title: "Sessions-2", description: "Sessions-2 Description" },
-            { title: "Sessions-3", description: "Sessions-3 Description" }
-        ]
-    })
-});
-
-sessionsRouter.route("/1").get((req, res) => {
-    res.send("single sessions");
-})
 
 app.use('/sessions', sessionsRouter);
 
